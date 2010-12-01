0d475967caab4b93f3766d50e2760a999b5cb671
Dirk Olbertz <dirk.olbertz@gmail.com>
2010-12-01 08:23:38 +0100

    Blocken von Adminseiten verhindern

--- wp-jmstv.php
+++ wp-jmstv.php
@@ -4,7 +4,7 @@
 Plugin Name: wp-JMStV
 Plugin URI: http://olbertz.de/blog/jmstv
 Description: Zeit- und IP-Sperre im Sinne des Jugendmedienschutzstaatsvertrag (JMStV)
-Version: 0.6
+Version: 0.7
 Author: Dirk Olbertz
 Author URI: http://olbertz.de
 License: GPL2
@@ -14,7 +14,9 @@ include('includes/DeJmstvPlugIn.php');
 
 $deJmstvPlugIn = new DeJmstvPlugIn();
 
-add_action('wp', array($deJmstvPlugIn, 'checkConstraints'));
+if (!is_admin()) {
+    add_action('wp', array($deJmstvPlugIn, 'checkConstraints'));
+}
 if (is_admin()) {
     add_option('jmstvBlockByIp', 0);
     add_option('jmstvBlockByUserAgent', 0);
